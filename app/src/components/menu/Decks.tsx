48b05cba3f7cd1324370fe7a0c9f5d8eb996c2b4
Bozhidar Panayotov <62803702+BojidarPanaiotov@users.noreply.github.com>
2024-06-27 16:01:42 +0300

    Fix menu z-index, print demo cards for development purposes, add center global class

--- app/src/components/menu/Decks.tsx
+++ app/src/components/menu/Decks.tsx
@@ -1,9 +1,24 @@
-const Decks = () => {
-  return (
-    <div>
-      <h1>Decks Section</h1>
-    </div>
+import Card from '../game/Card'
+
+const CardsMarkup = []
+
+for (let index = 0; index < 100; index++) {
+  CardsMarkup[index] = (
+    <Card
+      cost={Math.floor(Math.random() * 12) + 1}
+      attack={Math.floor(Math.random() * 20) + 1}
+      health={Math.floor(Math.random() * 20) + 1}
+      defense={Math.floor(Math.random() * 20) + 1}
+      name='Dragon Knight'
+      description='A mighty warrior with the power of dragons.'
+      type='Warrior'
+      image='https://cdn.midjourney.com/2575c6e0-451d-405a-8d38-e8632c7ed148/0_0.png'
+    />
   )
+}
+
+const Decks = () => {
+  return <div className='center'>{CardsMarkup}</div>
 }
 
 export default Decks

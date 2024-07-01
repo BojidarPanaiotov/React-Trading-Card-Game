2b41b36ce627dc6992a8fb0297e06614b11a73f3
Bozhidar Panayotov <62803702+BojidarPanaiotov@users.noreply.github.com>
2024-07-01 11:02:03 +0300

    Now long title cards are set to have lower font size

--- app/src/components/game/Card.tsx
+++ app/src/components/game/Card.tsx
@@ -1,11 +1,25 @@
 import '../styles/Card.css'
 
 const Card = ({ cost, attack, health, defense, name, description, type, image }) => {
+  let nameFontSize;
+
+  if (name.length <= 16) {
+    nameFontSize = '28px'
+  } else if (name.length <= 18) {
+    nameFontSize = '24px'
+  } else if (name.length <= 22) {
+    nameFontSize = '20px'
+  } else {
+    nameFontSize = '16px'
+  }
+
   return (
     <div className='tcg-card'>
       <div className='tcg-card-header'>
         <div className='tcg-card-cost'>{cost}</div>
-        <h2 className='tcg-card-name'>{name}</h2>
+        <h2 className='tcg-card-name' style={{ fontSize: nameFontSize }}>
+          {name}
+        </h2>
       </div>
       <div className='tcg-card-image'>
         <img src={image} alt={name} />

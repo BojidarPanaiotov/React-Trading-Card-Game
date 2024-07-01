eb2d20d3b657fa35b89561d94d0edd60bae34ece
Bozhidar Panayotov <62803702+BojidarPanaiotov@users.noreply.github.com>
2024-07-01 15:02:34 +0300

    Fix CSS issue with flex items in decks menu page, refactor Decks component

--- app/src/components/menu/Decks.tsx
+++ app/src/components/menu/Decks.tsx
@@ -1,24 +1,52 @@
 import Card from '../game/Card'
 
-const CardsMarkup = []
+import { CardTypeColors } from '../../constants/cardTypes'
 
-for (let index = 0; index < 100; index++) {
-  CardsMarkup[index] = (
-    <Card
-      cost={Math.floor(Math.random() * 12) + 1}
-      attack={Math.floor(Math.random() * 20) + 1}
-      health={Math.floor(Math.random() * 20) + 1}
-      defense={Math.floor(Math.random() * 20) + 1}
-      name='Dragon Knight'
-      description='A mighty warrior with the power of dragons.'
-      type='Warrior'
-      image='https://cdn.midjourney.com/2575c6e0-451d-405a-8d38-e8632c7ed148/0_0.png'
-    />
+const cards = [
+  <Card
+    cost={5}
+    attack={5}
+    health={1}
+    defense={10}
+    name='Titan Colossus'
+    description='A mighty titan'
+    type={CardTypeColors.find(card => card.type === 'Titan')}
+    image='https://cdn.midjourney.com/78d9a5d7-a83c-4c32-8ccb-c8d064f89050/0_0.png'
+  />,
+]
+
+const Decks = () => {
+  return (
+    <div className='center'>
+      <ul style={styles.list}>
+        {CardTypeColors.map(({ type, color }) => (
+          <li key={type} style={{ ...styles.listElement, backgroundColor: color }}>
+            {type}
+          </li>
+        ))}
+      </ul>
+      {cards}
+    </div>
   )
 }
 
-const Decks = () => {
-  return <div className='center'>{CardsMarkup}</div>
+const styles = {
+  list: {
+    listStyle: 'none',
+    display: 'flex',
+    columnGap: '1rem',
+    margin: '0',
+  },
+  listElement: {
+    color: 'white',
+    padding: '1rem',
+    borderRadius: '5px',
+    fontSize: '1rem',
+    textTransform: 'uppercase',
+    display: 'flex',
+    justifyContent: 'center',
+    cursor: 'pointer',
+  },
 }
 
 export default Decks

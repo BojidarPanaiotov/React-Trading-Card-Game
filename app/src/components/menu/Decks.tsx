e4d277ba773b42ef87bb557b0bdba5e691d3f21c
Bozhidar Panayotov <62803702+BojidarPanaiotov@users.noreply.github.com>
2024-07-10 14:48:32 +0300

    Move styling into separate file

--- app/src/components/menu/Decks.tsx
+++ app/src/components/menu/Decks.tsx
@@ -2,15 +2,18 @@ import Card from '../game/Card'
 
 import { CardTypeColors } from '../../constants/cardTypes'
 
+import '../styles/Decks.css'
+
 const cards = [
   <Card
+    key={'Titan Colossus'}
     cost={5}
     attack={5}
     health={1}
     defense={10}
     name='Titan Colossus'
     description='A mighty titan'
-    type={CardTypeColors.find(card => card.type === 'Titan')}
+    type={CardTypeColors.find((card) => card.type === 'Titan')}
     image='https://cdn.midjourney.com/78d9a5d7-a83c-4c32-8ccb-c8d064f89050/0_0.png'
   />,
 ]
@@ -18,9 +21,14 @@ const cards = [
 const Decks = () => {
   return (
     <div className='center'>
-      <ul style={styles.list}>
-        {CardTypeColors.map(({ type, color }) => (
-          <li key={type} style={{ ...styles.listElement, backgroundColor: color }}>
+      <ul className='list'>
+        {CardTypeColors.map(({ type, color }, index) => (
+          <li
+            key={index}
+            style={{
+              backgroundColor: color,
+            }}
+          >
             {type}
           </li>
         ))}
@@ -28,25 +36,6 @@ const Decks = () => {
       {cards}
     </div>
   )
-}
-
-const styles = {
-  list: {
-    listStyle: 'none',
-    display: 'flex',
-    columnGap: '1rem',
-    margin: '0',
-  },
-  listElement: {
-    color: 'white',
-    padding: '1rem',
-    borderRadius: '5px',
-    fontSize: '1rem',
-    textTransform: 'uppercase',
-    display: 'flex',
-    justifyContent: 'center',
-    cursor: 'pointer',
-  },
 }
 
 export default Decks
